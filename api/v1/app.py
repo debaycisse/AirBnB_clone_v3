d9a31cd1c2618f1006b53e1fbfefd0966cef261e
Azeez Adebayo <debaycisse@gmail.com>
2024-05-27 06:22:50 +0100

    work in progress

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module that defines a flask application whose views have been seperated"""
+
+from flask import Flask
+from models import storage
+from api.v1.view import app_views
+from os import getenv
+
+app = Flask(__name__)
+host_ip = getenv('HBNB_API_HOST', '0.0.0.0')
+port = getenv('HBNB_API_PORT', 5000)
+
+@app.teardown_appcontext
+def close_db(error):
+    """closes the database connection after each request
+
+    Args:
+        error - a returned error from any given request
+    """
+    storage.close()
+
+app.register_blueprint(app_views)
+
+
+if __name__ == '__main__':
+    app.run(host=host_ip, port=port, threaded=True)
