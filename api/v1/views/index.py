c0aeb0c8f9cf8e3cd0217071792e203cc7d30862
Azeez Adebayo <debaycisse@gmail.com>
2024-05-27 15:16:56 +0100

    implement task 4. Some stats?
    implement a route, based on blueprint and it returns the count of all
    instances using the count method of the storage instance

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,13 @@
 
 from flask import jsonify, request
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
@@ -12,5 +19,24 @@ def status():
         return (
                 jsonify(
                     {"status": "OK"}
+                )
+        )
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """retrieves the number of each objects by type"""
+    if (request.method == 'GET'):
+        amenity_count = storage.count(Amenity)
+        city_count = storage.count(City)
+        place_count = storage.count(Place)
+        review_count = storage.count(Review)
+        state_count = storage.count(State)
+        user_count = storage.count(User)
+        return (
+                jsonify(
+                    {"amenities": amenity_count, "cities": city_count,
+                     "places": place_count, "reviews": review_count,
+                     "states": state_count, "users": user_count}
                 )
         )
